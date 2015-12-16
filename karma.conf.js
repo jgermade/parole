32b7c48f6f96f63d246c7cb9678c6f48c507fd6a
Jesús Germade <jesus@germade.es>
2015-12-16 01:58:10 +0100

    passing tests in node and browsers

---
+++ karma.conf.js
@@ -0,0 +1,37 @@
+module.exports = function(config) {
+  'use strict';
+
+  var configuration = {
+    frameworks: ['mocha', 'chai'],
+    plugins: [
+      'karma-mocha',
+      'karma-chai',
+      'karma-chrome-launcher',
+      'karma-firefox-launcher',
+      'karma-story-reporter'
+    ],
+    files: [
+      'dist/promise.js',
+      'tests/{,**/}*.js'
+    ],
+    browsers: [ 'Chrome', 'Firefox' ],
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
+    singleRun: true,
+    reporters: ['story']
+  };
+
+  if(process.env.TRAVIS){
+    configuration.browsers = [ 'Chrome_travis_ci', 'Firefox' ];
+  }
+
+  if(process.env.DRONE){
+    configuration.browsers = [ 'Chrome' ];
+  }
+
+  config.set(configuration);
+};
