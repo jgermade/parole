b12f08564482390b1cc52e1b0e4fc19ceaa6c0d3
Jesús Germade <jesus@germade.es>
2015-12-16 01:00:11 +0100

    adding pkg:increaseVersion task

--- make.js
+++ make.js
@@ -14,4 +14,20 @@ require('nitro')(function (nitro) {
 
   });
 
+  var pkgActions = {
+    increaseVersion: function () {
+      nitro.package('bower').setVersion( nitro.package('npm').increaseVersion().version() );
+    }
+  };
+
+  nitro.task('pkg', function (target) {
+    if( pkgActions[target] ) {
+      return pkgActions[target]();
+    }
+
+    var pkg = require('./package');
+    process.stdout.write(pkg[target]);
+    process.exit(0);
+  });
+
 }).run();
