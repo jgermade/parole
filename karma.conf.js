de1c9d9eb94a4b61dc4464f05958077fa7d601de
Jesús Germade <jesus@germade.es>
2016-04-13 02:15:51 +0200

    trying firefox in drone

--- karma.conf.js
+++ karma.conf.js
@@ -30,7 +30,7 @@ module.exports = function(config) {
   }
 
   if(process.env.DRONE){
-    configuration.browsers = [ 'Chrome' ];
+    configuration.browsers = [ 'Firefox' ];
   }
 
   if(process.env.WERCKER){
