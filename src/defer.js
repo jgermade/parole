c383f4036a6b23a8c526d260addef7a43fc7655c
Jesús Germade <jesus@germade.es>
2018-02-17 15:14:04 +0100

    wip perf

--- src/defer.js
+++ src/defer.js
@@ -1,13 +1,24 @@
 
 module.exports = defer;
 
-const runImmediatelly = typeof process === 'object' && typeof process.nextTick === 'function' ? process.nextTick : ( typeof setImmediate === 'function' ? setImmediate : setTimeout );
+const nextTick = typeof process === 'object' && typeof process.nextTick === 'function' ?
+  process.nextTick :
+  (function(global, nextTick, process, prefixes, i, fn) {
+    // if( typeof global.process === 'object' && typeof global.process.nextTick === 'function' ) return global.process.nextTick;
+    for( i = prefixes.length - 1; i >= 0 ; i-- ) {
+      fn = global[prefixes[i++] + 'equestAnimationFrame'];
+      if( fn instanceof Function ) return fn;
+    }
+    return global.setImmediate || global.setTimeout;
+  })(typeof window === 'object' ? window : this, 'nextTick', 'process', 'oR msR mozR webkitR r'.split(' '), 0);
+
+// const runImmediatelly = typeof process === 'object' && typeof process.nextTick === 'function' ? process.nextTick : ( typeof setImmediate === 'function' ? setImmediate : setTimeout );
 
 const PENDING = -1;
 const FULFILLED = 0;
 const REJECTED = 1;
 
-function _runThen (then, fulfilled, value, resolve, reject) {
+function _runThen (p, then, fulfilled, value, resolve, reject) {
   var result;
   if( !( then instanceof Function ) ) {
     (fulfilled ? resolve : reject)(value);
@@ -15,7 +26,8 @@ function _runThen (then, fulfilled, value, resolve, reject) {
   }
   try{
     result = then(value);
-    if( result && result.then ) _runThen(result.then, true, value, resolve, reject);
+    if( result === p ) throw new TypeError('A promise can not be resolved by itself');
+    if( result && result.then ) result.then.call(result, resolve, reject);
     else resolve(result);
   } catch(reason) {
     reject(reason);
@@ -34,27 +46,25 @@ function defer (execute) {
     if( status !== PENDING ) return;
     status = FULFILLED;
     value = result;
-    runImmediatelly(_runListeners);
+    nextTick(_runListeners);
   }, function (reason) {
     if( status !== PENDING ) return;
     status = REJECTED;
     value = reason;
-    runImmediatelly(_runListeners);
+    nextTick(_runListeners);
   });
 
   return {
     then: function (onFulfilled, onRejected) {
+      var p = this;
       return defer(function (resolve, reject) {
-        var fulfilled;
-        if( status !== PENDING ) {
-          fulfilled = status === FULFILLED;
-          runImmediatelly(function () {
-            _runThen( fulfilled ? onFulfilled : onRejected, fulfilled, value, resolve, reject );
-          });
-        } else listeners.push(function () {
+
+        function complete () {
           var fulfilled = status === FULFILLED;
-          _runThen( fulfilled ? onFulfilled : onRejected, fulfilled, value, resolve, reject );
-        });
+          _runThen(p,  fulfilled ? onFulfilled : onRejected, fulfilled, value, resolve, reject );
+        }
+        if( status !== PENDING ) nextTick(complete);
+        else listeners.push(complete);
       });
     },
     catch: function (onRejected) {
@@ -63,44 +73,46 @@ function defer (execute) {
   };
 }
 
-// defer.resolve = function (value) {
-//   return defer(function (resolve) {
-//     resolve(value);
-//   });
-// };
-//
-// defer.reject = function (reason) {
-//   return defer(function (_resolve, reject) {
-//     reject(reason);
-//   });
-// };
-
 defer.resolve = function (value) {
-  return {
-    then: function (onFulfilled) {
-      if( !(onFulfilled instanceof Function) ) return defer.resolve(value);
-      return defer(function (resolve, reject) {
-        runImmediatelly(function () {
-          _runThen( onFulfilled, true, value, resolve, reject );
-        });
-      });
-    },
-    catch: function () {}
-  };
+  return defer(function (resolve) {
+    resolve(value);
+  });
 };
 
 defer.reject = function (reason) {
-  return {
-    then: function (_onFulfilled, onRejected) {
-      return this.catch(onRejected);
-    },
-    catch: function (onRejected) {
-      if( !(onRejected instanceof Function) ) return defer.reject(reason);
-      return defer(function (resolve, reject) {
-        runImmediatelly(function () {
-          _runThen( onRejected, false, reason, resolve, reject );
-        });
-      });
-    },
-  };
+  return defer(function (_resolve, reject) {
+    reject(reason);
+  });
 };
+
+// defer.resolve = function (value) {
+//   return {
+//     then: function (onFulfilled) {
+//       var p = this;
+//       if( !(onFulfilled instanceof Function) ) return defer.resolve(value);
+//       return defer(function (resolve, reject) {
+//         nextTick(function () {
+//           _runThen( p, onFulfilled, true, value, resolve, reject );
+//         });
+//       });
+//     },
+//     catch: function () {}
+//   };
+// };
+//
+// defer.reject = function (reason) {
+//   return {
+//     then: function (_onFulfilled, onRejected) {
+//       return this.catch(onRejected);
+//     },
+//     catch: function (onRejected) {
+//       var p = this;
+//       if( !(onRejected instanceof Function) ) return defer.reject(reason);
+//       return defer(function (resolve, reject) {
+//         nextTick(function () {
+//           _runThen( p, onRejected, false, reason, resolve, reject );
+//         });
+//       });
+//     },
+//   };
+// };
--- src/parole.js
+++ src/parole.js
@@ -1,10 +1,16 @@
 
-var runImmediatelly = typeof process === 'object' && typeof process.nextTick === 'function' ? process.nextTick : ( typeof setImmediate === 'function' ? setImmediate : setTimeout );
-// const runImmediatelly = setTimeout;
+// const nextTick = typeof process === 'object' && typeof process.nextTick === 'function' ? process.nextTick : ( typeof setImmediate === 'function' ? setImmediate : setTimeout );
+const nextTick = typeof process === 'object' && typeof process.nextTick === 'function' ?
+  process.nextTick :
+  (function(global, nextTick, process, prefixes, i, fn) {
+    // if( typeof global.process === 'object' && typeof global.process.nextTick === 'function' ) return global.process.nextTick;
+    for( i = prefixes.length - 1; i >= 0 ; i-- ) {
+      fn = global[prefixes[i++] + 'equestAnimationFrame'];
+      if( fn instanceof Function ) return fn;
+    }
+    return global.setImmediate || global.setTimeout;
+  })(typeof window === 'object' ? window : this, 'nextTick', 'process', 'oR msR mozR webkitR r'.split(' '), 0);
 
-// function runImmediatelly (fn) {
-//   return _runImmediatelly(fn);
-// }
 
 var PENDING = -1;
 var FULFILLED = 0;
@@ -13,93 +19,23 @@ var REJECTED = 1;
 function _runThen (p, then, fulfilled, value, resolve, reject) {
   var result;
 
-  // if( p.status === PENDING ) throw new Error('promise PENDING');
-  // console.log('_runThen', typeof p, typeof value);
-  // if( value === p ) return reject(new TypeError('A promise can not be resolved by itself'));
+  if( !( then instanceof Function ) ) return (fulfilled ? resolve : reject)(value);
 
-  if( !( then instanceof Function ) ) {
-    return (fulfilled ? resolve : reject)(value);
-  }
   try{
-    // if( value === p ) throw new TypeError('A promise can not be resolved by itself');
     result = then(value);
     if( result === p ) throw new TypeError('A promise can not be resolved by itself');
-    // if( result && result.then instanceof Function ) _runThen(result, result.then, true, value, resolve, reject);
-    if( (typeof result === 'object' || result instanceof Function) && 'then' in result ) result.then(resolve, reject);
+
+    if( (typeof result === 'object' || result instanceof Function) && 'then' in result ) result.then.call(result, resolve, reject);
     else resolve(result);
-    // resolve(result);
+
   } catch (reason) {
     return reject( reason );
-    // if( reason && reason.then instanceof Function ) reason.then(resolve, reject);
-    // else reject( reason );
-    // if( reason && reason.then instanceof Function ) _runThen(reason, reason.then, false, value, resolve, reject);
-    // else reject( reason );
-
-    // if( result === this )
-    // reject( result === p ? new TypeError('A promise can not be resolved by itself') : reason );
-
   }
-
-  // var _then = result && result.then;
-  //
-  // if( _then ) {
-  //   try {
-  //     _then(resolve, reject);
-  //   } catch(_err) {
-  //     reject(_err);
-  //   }
-  // } else resolve(result);
-
-  // var _then = result && result.then;
-  //
-  // if( _then instanceof Function ) {
-  //   try {
-  //     _then(resolve, reject);
-  //   } catch(_err) {
-  //     reject(_err);
-  //   }
-  // } else resolve(result);
-
-  // if( result && 'then' in result ) {
-  //   try {
-  //     result.then(resolve, reject);
-  //   } catch(_err) {
-  //     reject(_err);
-  //   }
-  // } else resolve(result);
-  // if( result && result.then instanceof Function ) result.then(resolve, reject);
-  // else resolve(result);
 }
-
-// function _setPromiseValue (p, status, value) {
-//   // if( value === p || value.promise === p ) {
-//   if( value === p ) {
-//     // eslint-disable-next-line
-//     console.log('A promise can not be resolved by itself');
-//     status = REJECTED;
-//     value = new TypeError('A promise can not be resolved by itself');
-//   }
-//   Object.defineProperty(p, 'status', {
-//     enumerable: false,
-//     writable: false,
-//     value: status
-//   });
-//   Object.defineProperty(p, 'value', {
-//     enumerable: false,
-//     writable: false,
-//     value: value
-//   });
-// }
-
-// var _runListeners = function (listeners) {
-//   for( var i = 0, n = listeners.length ; i < n ; i++ ) listeners[i]();
-// };
 
 function Parole (runContext) {
   if( !(this instanceof Parole) ) return new Parole(runContext);
 
-  // eslint-disable-next-line
-  // console.log('Parole', this);
   var p = this;
   var listeners = [];
 
@@ -113,38 +49,18 @@ function Parole (runContext) {
 
   runContext.call(this, function (result) {
     if( p.status !== PENDING ) return;
-    // // _setPromiseValue(p, FULFILLED, result);
     p.status = FULFILLED;
     p.value = result;
-    runImmediatelly(_runListeners);
+    nextTick(_runListeners);
   }, function (reason) {
     if( p.status !== PENDING ) return;
-    // // _setPromiseValue(p, REJECTED, reason);
     p.status = REJECTED;
     p.value = reason;
-    runImmediatelly(_runListeners);
+    nextTick(_runListeners);
   });
-
-  // p.then = function (onFulfilled, onRejected) {
-  //   return new Parole(function (resolve, reject) {
-  //     var _p = this;
-  //     function complete () {
-  //       var fulfilled = (p.status === FULFILLED);
-  //       _runThen(_p, fulfilled ? onFulfilled : onRejected, fulfilled, p.value, resolve, reject );
-  //     }
-  //     if( p.status !== PENDING ) runImmediatelly(complete);
-  //     else listeners.push(complete);
-  //   });
-  // };
 }
 
 Parole.prototype.status = PENDING;
-// Object.defineProperty(Parole.prototype, 'status', {
-//   enumerable: false,
-//   configurable: false,
-//   writable: false,
-//   value: PENDING
-// });
 
 Parole.prototype.then = function (onFulfilled, onRejected) {
   // // eslint-disable-next-line
@@ -159,48 +75,11 @@ Parole.prototype.then = function (onFulfilled, onRejected) {
     // console.log('Parole.prototype.then', _p );
 
     function complete () {
-      var result,
-          fulfilled = p.status === FULFILLED,
-          then = fulfilled ? onFulfilled : onRejected;
-
-      // if( p.status === PENDING ) throw new Error('promise PENDING');
-      // console.log('_runThen', typeof p, typeof value);
-      // if( value === p ) return reject(new TypeError('A promise can not be resolved by itself'));
-
-      if( !( then instanceof Function ) ) {
-        return (fulfilled ? resolve : reject)(p.value);
-      }
-      try{
-        // if( value === p ) throw new TypeError('A promise can not be resolved by itself');
-        result = then(p.value);
-        if( result === _p ) throw new TypeError('A promise can not be resolved by itself');
-        // if( result && result.then instanceof Function ) _runThen(result, result.then, true, value, resolve, reject);
-        if( (typeof result === 'object' || result instanceof Function) && 'then' in result ) result.then(resolve, reject);
-        else resolve(result);
-        // resolve(result);
-      } catch (reason) {
-        return reject( reason );
-        // if( reason && reason.then instanceof Function ) reason.then(resolve, reject);
-        // else reject( reason );
-        // if( reason && reason.then instanceof Function ) _runThen(reason, reason.then, false, value, resolve, reject);
-        // else reject( reason );
-
-        // if( result === this )
-        // reject( result === p ? new TypeError('A promise can not be resolved by itself') : reason );
-
-      }
-
-      // var fulfilled = p.status === FULFILLED;
-      // // console.log('Parole.prototype.then', _p.value );
-      // // if( p.status === PENDING ) {
-      // //   throw new Error(p);
-      // //   process.exit(1);
-      // // }
-      // _runThen(_p, fulfilled ? onFulfilled : onRejected, fulfilled, p.value, resolve, reject );
+      var fulfilled = p.status === FULFILLED;
+      _runThen(_p, fulfilled ? onFulfilled : onRejected, fulfilled, p.value, resolve, reject );
     }
-    if( p.status !== PENDING ) runImmediatelly(complete);
+    if( p.status !== PENDING ) nextTick(complete);
     else p.listeners.push(complete);
-    // else p.listeners.push(complete);
   });
 };
 
@@ -220,13 +99,49 @@ Parole.reject = function (reason) {
   });
 };
 
+Parole.defer = function () {
+  var deferred = {};
+  deferred.promise = new Parole(function (resolve, reject) {
+    deferred.resolve = resolve;
+    deferred.reject = reject;
+  });
+  return deferred;
+};
+
+Parole.all = function (promises) {
+  var waiting_promises = promises.length;
+  return new Promise(function (resolve, reject) {
+    var results = new Array(waiting_promises);
+    promises.forEach(function (promise, i) {
+      var addresult = function (result) {
+        results[i] = result;
+        waiting_promises--;
+        if( !waiting_promises ) resolve(results);
+      };
+      if( (typeof promise === 'object' || promise instanceof Function) && 'then' in promise ) {
+        promise.then.call(promise, addresult, reject);
+      } else addresult(promise);
+    });
+  });
+};
+
+Parole.race = function (promises) {
+  return new Parole(function (resolve, reject) {
+    promises.forEach(function (promise) {
+      if( (typeof promise === 'object' || promise instanceof Function) && 'then' in promise ) {
+        promise.then.call(promise, resolve, reject);
+      } else resolve(promise);
+    });
+  });
+};
+
 // Parole.resolve = function (value) {
 //   return {
 //     then: function (onFulfilled) {
 //       if( !(onFulfilled instanceof Function) ) return Parole.resolve(value);
 //       return new Parole(function (resolve, reject) {
 //         var p = this;
-//         runImmediatelly(function () {
+//         nextTick(function () {
 //           _runThen( p, onFulfilled, true, value, resolve, reject );
 //         });
 //       });
@@ -244,21 +159,12 @@ Parole.reject = function (reason) {
 //       if( !(onRejected instanceof Function) ) return Parole.reject(reason);
 //       return new Parole(function (resolve, reject) {
 //         var p = this;
-//         runImmediatelly(function () {
+//         nextTick(function () {
 //           _runThen( p, onRejected, false, reason, resolve, reject );
 //         });
 //       });
 //     },
 //   };
 // };
-
-Parole.defer = function () {
-  var deferred = {};
-  deferred.promise = new Parole(function (resolve, reject) {
-    deferred.resolve = resolve;
-    deferred.reject = reject;
-  });
-  return deferred;
-};
 
 module.exports = Parole;
