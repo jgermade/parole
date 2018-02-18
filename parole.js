e3c65f7985898c5290a997d1bf9d573820e94074
Jesús Germade <jesus@germade.es>
2018-02-18 15:11:13 +0100

    WIP Message channel
    * wip MessageChannel for nextTick
    
    * cheat

--- parole.js
+++ parole.js
@@ -15,7 +15,20 @@
 
   var nextTick = typeof process === 'object' && typeof process.nextTick === 'function' ? process.nextTick :
       (function (global) {
-        return 'MutationObserver' in global ? (function (node) {
+        if( 'Promise' in global && typeof global.Promise.resolve === 'function' ) return (function (resolved) {
+          return function (fn) {
+            resolved.then(fn);
+          };
+        })( global.Promise.resolve() );
+        // if( 'MessageChannel' in global ) return (function (mc) {
+        //   var callback;
+        //   mc.port1.onmessage = function () { callback(); };
+        //   return function (_callback) {
+        //     callback = _callback;
+        //     mc.port2.postMessage(0);
+        //   };
+        // })( new MessageChannel() );
+        if( 'MutationObserver' in global ) return (function (node) {
           return function (callback) {
             var observer = new MutationObserver(function () {
               callback();
@@ -24,7 +37,8 @@
             observer.observe( node, {characterData: true} );
             node.data = false;
           };
-        })( document.createTextNode('') ) : ( global.setImmediate || global.setTimeout );
+        })( document.createTextNode('') );
+        return global.setImmediate || global.setTimeout;
       })( typeof window === 'object' ? window : this );
 
   function once (fn) {
